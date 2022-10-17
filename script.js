a3a946f3a346c6e73924ca5674747b22781f7854
ConstHrach <hrachya62799@gmail.com>
2022-10-17 14:59:07 -0700

    Cards made into an array for cleaner code, notes added

--- script.js
+++ script.js
@@ -1,84 +1,43 @@
 // Deck of cards
 
-const deckOfCards = [
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; //the combination of the two arrays creates a 52 card deck
+var deck = new Array();
 
-    // need to implement a way for aces number value to be 1 or 11 based on card 1 plus card 2's value, also deckOfCards code will be refined to cleaner code. This is just for visualization 
-    { card: "King", numberValue: 10, suit: "Hearts" },
-    { card: "Queen", numberValue: 10, suit: "Hearts" },
-    { card: "Jack", numberValue: 10, suit: "Hearts" },
-    { card: "Ten", numberValue: 10, suit: "Hearts" },
-    { card: "Nine", numberValue: 9, suit: "Hearts" },
-    { card: "Eight", numberValue: 8, suit: "Hearts" },
-    { card: "Seven", numberValue: 7, suit: "Hearts" },
-    { card: "Six", numberValue: 6, suit: "Hearts" },
-    { card: "Five", numberValue: 5, suit: "Hearts" },
-    { card: "Four", numberValue: 4, suit: "Hearts" },
-    { card: "Three", numberValue: 3, suit: "Hearts" },
-    { card: "Two", numberValue: 2, suit: "Hearts" },
-    { card: "One", numberValue: 1, suit: "Hearts" },
-
-    { card: "King", numberValue: 10, suit: "Spades" },
-    { card: "Queen", numberValue: 10, suit: "Spades" },
-    { card: "Jack", numberValue: 10, suit: "Spades" },
-    { card: "Ten", numberValue: 10, suit: "Spades" },
-    { card: "Nine", numberValue: 9, suit: "Spades" },
-    { card: "Eight", numberValue: 8, suit: "Spades" },
-    { card: "Seven", numberValue: 7, suit: "Spades" },
-    { card: "Six", numberValue: 6, suit: "Spades" },
-    { card: "Five", numberValue: 5, suit: "Spades" },
-    { card: "Four", numberValue: 4, suit: "Spades" },
-    { card: "Three", numberValue: 3, suit: "Spades" },
-    { card: "Two", numberValue: 2, suit: "Spades" },
-    { card: "One", numberValue: 1, suit: "Spades" },
-
-    { card: "King", numberValue: 10, suit: "Clubs" },
-    { card: "Queen", numberValue: 10, suit: "Clubs" },
-    { card: "Jack", numberValue: 10, suit: "Clubs" },
-    { card: "Ten", numberValue: 10, suit: "Clubs" },
-    { card: "Nine", numberValue: 9, suit: "Clubs" },
-    { card: "Eight", numberValue: 8, suit: "Clubs" },
-    { card: "Seven", numberValue: 7, suit: "Clubs" },
-    { card: "Six", numberValue: 6, suit: "Clubs" },
-    { card: "Five", numberValue: 5, suit: "Clubs" },
-    { card: "Four", numberValue: 4, suit: "Clubs" },
-    { card: "Three", numberValue: 3, suit: "Clubs" },
-    { card: "Two", numberValue: 2, suit: "Clubs" },
-    { card: "One", numberValue: 1, suit: "Clubs" },
-
-    { card: "King", numberValue: 10, suit: "Diamonds" },
-    { card: "Queen", numberValue: 10, suit: "Diamonds" },
-    { card: "Jack", numberValue: 10, suit: "Diamonds" },
-    { card: "Ten", numberValue: 10, suit: "Diamonds" },
-    { card: "Nine", numberValue: 9, suit: "Diamonds" },
-    { card: "Eight", numberValue: 8, suit: "Diamonds" },
-    { card: "Seven", numberValue: 7, suit: "Diamonds" },
-    { card: "Six", numberValue: 6, suit: "Diamonds" },
-    { card: "Five", numberValue: 5, suit: "Diamonds" },
-    { card: "Four", numberValue: 4, suit: "Diamonds" },
-    { card: "Three", numberValue: 3, suit: "Diamonds" },
-    { card: "Two", numberValue: 2, suit: "Diamonds" },
-    { card: "One", numberValue: 1, suit: "Diamonds" },
-
-];
+function createDeck() {
+    deck = new Array();
+    for (var i = 0; i < values.length; i++) { // determining the card
+        for (var x = 0; x < suits.length; x++) { // determining the suit
+            var weight = parseInt(values[i]);
+            if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                weight = 10;
+            if (values[i] == "A")
+                if (sum <= 21) { //making the value of the ace 1 or 11 depending on the sum of the cards
+                    weight = 11;
+                }
+                else {
+                    weight = 1;
+                }
+            var card = { Value: values[i], Suit: suits[x], Weight: weight };
+            deck.push(card);
+        }
+    }
+}
 
 
-/*
 
+//Necessary Phases planned out
 
-Necessary Phases planned out
-
-Is shuffling needed???
-
-Initialize: DealPhase
-Dispose: DealPhase
-Break
-Initialize: MainDecisionPhase (choose to hit or stay)
-Dispose: MainDecisionPhase
-Break
-Initialize: DealerPlayPhase (dealer chooses to hit or stay depending on card value)
-Dispose: DealerPlayPhase
-Break
-Initialize: ScoringPhase (checks playerScore vs dealerScore and decides winner. If(playerScore > dealerScore && playerScore <= 21)
-dipslay "You win")
-Dispose: ScoringPhase
-*/
+// Is shuffling needed???
+// Initialize: DealPhase
+// Dispose: DealPhase
+// Break
+// Initialize: MainDecisionPhase (choose to hit or stay)
+// Dispose: MainDecisionPhase
+// Break
+// Initialize: DealerPlayPhase (dealer chooses to hit or stay depending on card value)
+// Dispose: DealerPlayPhase
+// Break
+// Initialize: ScoringPhase (checks playerScore vs dealerScore and decides winner. If(playerScore > dealerScore && playerScore <= 21)
+// dipslay "You win")
+// Dispose: ScoringPhase
